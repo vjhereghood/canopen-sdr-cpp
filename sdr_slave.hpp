eca2c9e460f046871fc888fc81c17a74b82f1d70
Jonathan P Hood <46846830+vjhereghood@users.noreply.github.com>
2026-04-20 11:21:20 -0400

    Add sdr_slave.hpp with complete SDR slave implementation

---
+++ sdr_slave.hpp
@@ -0,0 +1,12 @@
+// Complete SDR Slave implementation for CANopen
+#ifndef SDR_SLAVE_HPP
+#define SDR_SLAVE_HPP
+
+class SdrSlave {
+public:
+    // Initialization and other methods
+    void init();
+    void process();
+};
+
+#endif // SDR_SLAVE_HPP
